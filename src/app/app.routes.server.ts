289fa2ef37018dd36698b7f933611a1a32d93d28
Diallo <adramane2707@gmail.com>
2026-05-30 02:58:44 +0200

    build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'profil/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'devis-request/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
